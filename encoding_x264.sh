b6f4c7f9d74fbea493db168693fca7335738c4e6
charlottalph <charlottalph@gmail.com>
2020-07-22 16:40:07 +1200

    x264 transcoding ver1

--- encoding_x264.sh
+++ encoding_x264.sh
@@ -41,10 +41,15 @@ V_SIZE_6=1920x1080
 #     -hls_segment_filename HLS/stream_%v/s%06d.ts \
 #     -strftime_mkdir 1 \
 #     -var_stream_map "v:0,a:0 v:1,a:1 v:2,a:2 v:3,a:3 v:4,a:4 v:5,a:5 v:6,a:6 v:7,a:7 v:8,a:8" HLS/stream_%v.m3u8
+   -master_pl_name $VIDEO_OUT.m3u8 \
+    -hls_segment_filename HLS/stream_%v/s%06d.ts \
+    -strftime_mkdir 1 \
+    -var_stream_map "v:0,a:0 v:1,a:1 v:2,a:2 v:3,a:3 v:4,a:4 v:5,a:5 v:6,a:6 v:7,a:7 v:8,a:8" HLS/stream_%v.m3u8
+
 
 mkdir -p ${title}_HLS
 
-${ffcmd} -hide_banner -threads 8 -i $VIDEO_IN -y \
+${ffcmd} -hide_banner -y -threads 8 -i $VIDEO_IN \
     -preset $PRESET_P -keyint_min $GOP_SIZE -g $GOP_SIZE -sc_threshold 0 -r $FPS -c:v libx264 -pix_fmt yuv420p \
     -map v:0 -s:0 $V_SIZE_1 -b:v:0 144k -r:v:0 15 -maxrate:0 180k -bufsize:0 320k \
     -map v:0 -s:1 $V_SIZE_2 -b:v:1 180k -maxrate:1 210k -bufsize:1 360k \
@@ -56,6 +61,6 @@ ${ffcmd} -hide_banner -threads 8 -i $VIDEO_IN -y \
     -c:a libfdk_aac -ac 2 -b:a 128k -r:a 44100 \
     -f hls -hls_time $HLS_TIME -hls_playlist_type vod -hls_flags independent_segments \
     -master_pl_name $VIDEO_OUT.m3u8 \
-    -hls_segment_filename ${title}_HLS/stream_%v/s%06d.ts \
+    -hls_segment_filename HLS/${title}_stream_%v/s%06d.ts \
     -strftime_mkdir 1 \
-    -var_stream_map "v:0,a:0 v:1,a:1 v:2,a:2 v:3,a:3 v:4,a:4 v:5,a:5" ${title}_HLS/stream_%v.m3u8
+    -var_stream_map "v:0,a:0 v:1,a:1 v:2,a:2 v:3,a:3 v:4,a:4 v:5,a:5" ${title}_stream_%v.m3u8
